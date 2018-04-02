b1cd63e015643000a075f008b044d08ebea25b3d
Zack Katz <zack@katz.co>
2018-04-02 16:51:02 -0600

    Fix PHP 7 incompatibility
    Get rid of eregi() code

--- api/cc_class.php
+++ api/cc_class.php
@@ -79,7 +79,7 @@
         * @return  TRUE if address is valid and FALSE if not.
         */
         protected function isValidEmail($email){
-             return eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $email);
+	        return is_email( $email );
         }
 
         /**
